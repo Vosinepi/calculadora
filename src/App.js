0845526d3cc253dfdf72913dbec39540b44de710
Vosinepi <85812110+Vosinepi@users.noreply.github.com>
2022-04-06 21:58:14 -0300

    final final

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
   console.log('este numero' + input.charAt(input.length - 4))
   const agregarInput = (val) => {
     var ultimoValor = String(input.charAt(input.length - 1))
+    var ultimoValorMenosDos = String(input.charAt(input.length - 3))
+    console.log(ultimoValorMenosDos)
     if (input === '0') {
       setInput(val)
     } else {
@@ -22,15 +24,24 @@ function App() {
         (val === '*' || val === '/') &&
         (ultimoValor === '/' || ultimoValor === '*')
       ) {
-        alert(
-          'Estas queriendo agregar un doble operador que no esta permitido1'
-        )
+        setInput(input.slice(0, -1) + val)
+        // console.log("Este es el nuevo input con el operador "+ input)
+        //   alert(
+        //     'Estas queriendo agregar un doble operador que no esta permitido1'
+        //   )
       } else if (val === '+' && input.charAt(input.length - 1) === '+') {
-        alert(
-          'Estas queriendo agregar un doble operador que no esta permitido2'
-        )
+        setInput(input.slice(0, -1) + val) // alert(
+        //     'Estas queriendo agregar un doble operador que no esta permitido2'
+        //   )
       } else if (val === '+' && input.charAt(input.length - 1) === '-') {
         setInput(input + val)
+      } else if (
+        //"5 * - + 5"
+        input.charAt(input.length - 2) === '-' &&
+        (ultimoValorMenosDos === '*' || ultimoValorMenosDos === '/')
+      ) {
+        console.log(input.substring(0, 1) + '' + input.substring(1 + 1))
+        setInput(input.substring(0, 1) + '' + input.substring(2 + 1) + val)
       } else {
         setInput(input + val)
       }
